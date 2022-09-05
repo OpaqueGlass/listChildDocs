e33126ed59d829ef402581c7ddfe956b2db918ff
OpaqueGlass <52985952+OpaqueGlass@users.noreply.github.com>
2022-09-05 11:13:07 +0800

    comment. 注释更改

--- src/config.js
+++ src/config.js
@@ -24,8 +24,8 @@ let setting = {//全局设置
     divideColumnAtIndent: false, //分列截断方式（仅用于写入文档模式：url、引用块）为true: 多层级时，在缩进处截断，使每列行数相同，但层级>=2时体验不佳; 为false，按照第一层级分列，每列行数不等
     emojiEnable: true, //为true则一并写入文档icon Emoji
     floatWindowEnable: true,//为true启用挂件内浮窗（beta）
-    includeAttrName: ["custom-f"],//数组中的属性名将在更新目录列表块时一并写入无序列表对应的dom，或许可以适配部分主题的无序列表转导图功能
-    superBlockBeta: false,//使用玄学的超级块创建方式//请设置为false，dev用
+    includeAttrName: ["custom-f"],//数组中的属性名将在更新目录列表块时一并写入无序列表对应的元素属性中，或许可以适配部分主题的无序列表转导图功能
+    superBlockBeta: true,//使用玄学的超级块创建方式//如果出现问题，请设置为false（测试中）
 };
 //全局设置
 let includeOs = ["windows"];//监视页签变化自动刷新功能将在列出的操作系统上启用
